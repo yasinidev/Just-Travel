b4b48b1fa29f0136f7593d62c0bb28740fa3c2cc
yasiniweb <yasini4real@gmail.com>
2022-08-15 15:02:31 +0200

    Create admin2.php

---
+++ admin2.php
@@ -0,0 +1,210 @@
+<?php
+include_once('db.php');
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Just Travel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="admin1.php" style="font-size: 27px;">Just Travel Agency</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li><a href="admin1.php" style="font-size: 27px;">Tour Requests</a></li>
+								<li class="active"><a href="admin2.php" style="font-size: 27px;">Hotel Requests</a></li>
+								<li><a href="admin3.php" style="font-size: 27px;">Car Requests</a></li>
+								<li><a href="admin4.php" style="font-size: 27px;">Contacts</a></li>
+								<li><a href="index.php" style="font-size: 27px;">Log Out</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/admin.jpeg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>Admin Panel</h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+
+		<div id="colorlib-contact">
+			<h1>Hotel Booking Requests</h1>
+						<table id="myTable" class="table table-striped" >  
+				<thead>
+					<th>NAMES</th>
+					<th>PHONE</th>
+					<th>EMAIL</th>
+					<th><center>NATIONALITY</center></th>
+					<th>CHECK-IN</th>
+					<th>CHECK-OUT</th>
+					<th>ADULTS</th>
+					<th>CHILDREN</th>
+					<th>DESTINATION</th>
+					<th>BUDGET</th>
+					<th>OTHER ENQUIRY</th>
+				</thead>
+				<tbody>
+					<?php 
+						$sql = "SELECT * FROM hotels";
+						$res = $conn->query($sql);
+
+						foreach ($res as $row) {
+							$fnames = $row['fnames'];
+							$phone = $row['phone'];
+							$email = $row['email'];
+							$nationality = $row['nationality'];
+							$checkin = $row['checkin'];
+							$checkout = $row['checkout'];
+							$adults = $row['adults'];
+							$children = $row['children'];
+							$destination = $row['destination'];
+							$budget = $row['budget'];
+							$enquiry = $row['enquiry'];
+					?>
+					<tr>
+						<td ><?php echo $fnames; ?></td>
+						<td ><?php echo $phone; ?></td>
+						<td><?php echo $email; ?></td>
+						<td><?php echo $nationality; ?></td>
+						<td><?php echo $checkin; ?></td>
+						<td><?php echo $checkout; ?></td>
+						<td><?php echo $adults; ?></td>
+						<td><?php echo $children; ?></td>
+						<td><?php echo $destination; ?></td>
+						<td><?php echo $budget; ?></td>
+						<td><?php echo $enquiry; ?></td>
+					</tr>
+					<?php } ?>
+
+				</tbody>
+
+
+				</table>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 animate-box">
+						
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		
+	</div>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Google Map -->
+	
+	<script src="js/google_map.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
