3c2254878831447b90651dbe3ce56dedf60a85cd
yasiniweb <yasini4real@gmail.com>
2022-08-15 15:02:25 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Just Travel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="index.php">Just Travel Agency</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li><a href="index.php" style="font-size: 27px;">Home</a></li>
+								<li><a href="services.php" style="font-size: 27px;">Services</a></li>
+								<li class="active"><a href="about.php" style="font-size: 27px;">About</a></li>
+								<li><a href="contact.php" style="font-size: 27px;">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/cover-img-6.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>About us</h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+		<div id="colorlib-about">
+			<div class="container">
+				<div class="row">
+					<div class="about-flex">
+						<div class="col-one-forth aside-stretch animate-box">
+							<div class="row">
+								<div class="col-md-12 about">
+									<h2>About</h2>
+
+									<ul>
+										<!--<li><a href="#">History</a></li>-->
+										<!--<li><a href="#">Staff</a></li>-->
+										<li><a href="contact.html">Connect with us</a></li>
+										<!--<li><a href="#">Faqs</a></li>-->
+										<!--<li><a href="#">Career</a></li>-->
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-three-forth animate-box">
+							<h2>History</h2>
+							<div class="row">
+								<div class="col-md-12">
+									 
+
+									<p>Just Travel & tour Agency is based in RWANDA offering tailor made tours for travellers seeking to explore RWANDA and its environs We specialize in a wide array of Tours from day trips, cultural tours, Multi – day tours, Private tours, Nature & wild Safaris and many more…</p>
+
+									<p>Whether on holiday or business, our tours are designed to work with your time and schedule to ensure you get a taste of RWANDA and its environs.</p>
+
+									<p> We pick you up and drop you from any accommodation venue in RWANDA and we allow any booking from small groups to big groups Our team is waiting for you to give you a service that will create memories of your visit.</p>
+
+									<div class="row row-pb-sm">
+										<div class="col-md-6">
+											<img class="img-responsive" src="images/" alt="">
+										</div>
+										<div class="col-md-6">
+											<p></p>
+										</div>
+									</div>
+
+
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<?php
+include('footer.html');
+?>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
