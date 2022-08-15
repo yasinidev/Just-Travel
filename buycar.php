3aa43a62dfe1acdef2d57847c07919f50d18fc20
yasiniweb <yasini4real@gmail.com>
2022-08-16 00:11:25 +0200

    Create buycar.php

---
+++ buycar.php
@@ -0,0 +1,326 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Just Travel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+	<?php
+	include('navbarservicespage.html');
+	?>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/carr.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>Buy Cars</h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   
+			  	</ul>
+		  	</div>
+		</aside>
+
+		<div class="colorlib-wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="wrap-division">
+									<div class="col-md-12 col-md-offset-0 heading2 animate-box">
+										<h2>Available Cars</h2>
+									</div>
+									<div class="row">
+
+										
+										<div class="col-md-12 animate-box">
+											<div class="room-wrap">
+												<div class="row">
+													<div class="col-md-6 col-sm-6">
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/cars/1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/cars/2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/cars/3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/cars/4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			  	</ul>
+		  	</div>
+		</aside>
+													</div>
+													<div class="col-md-6 col-sm-6">
+														<div class="desc">
+															<h2>Land Cruiser</h2>
+															<p class="price"><span>$3,500</span> <small>/ Negotiable</small></p>
+															<p><b>Brand Type:</b>  Toyota</p>
+															<p><b>Description:</b>  This car is in good condition, with Insurance and control</p>
+															<p><b>Model:</b>  Unknown</p>
+															<p><b>Transmission:</b>  Manuel</p>
+															<p><b>Fuel type:</b>  Essence/Petrol</p>
+															<p><b>Engine Cap:</b>  n/a</p>
+															<p><b>Registration Date:</b>  Unknown</p>
+															<p><b>Year of Manufacture:</b>  2000</p>
+															<p><b>Mileage:</b>  270,000</p>
+															<p><b>Features:</b>  Unknown</p>
+															<p><b>Accessoriese:</b>  Radio</p>
+															<p><a href="tel:+250782782300" class="btn btn-primary">Call Seller</a>
+																<a href="mailto:eric@just-travelagency.com" class="btn btn-primary">Email Seller</a>
+															<a href="https://api.whatsapp.com/send?phone=+250782782300&text=hello!%21%20i need%20to buy%20your car%20and need%20more information." class="btn btn-primary" target="_blank">
+															<i class="fa fa-whatsapp my-float">Whatsapp Seller</i>
+															</a></p>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+
+									
+
+<div class="col-md-12 animate-box">
+											<div class="room-wrap">
+												<div class="row">
+													<div class="col-md-6 col-sm-6">
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/cars/1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/cars/2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/cars/3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/cars/4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   
+			  	</ul>
+		  	</div>
+		</aside>
+													</div>
+													<div class="col-md-6 col-sm-6">
+														<div class="desc">
+															<h2>Land Cruiser</h2>
+															<p class="price"><span>$3,500</span> <small>/ Negotiable</small></p>
+															<p><b>Brand Type:</b>  Toyota</p>
+															<p><b>Description:</b>  This car is in good condition, with Insurance and control</p>
+															<p><b>Model:</b>  Unknown</p>
+															<p><b>Transmission:</b>  Manuel</p>
+															<p><b>Fuel type:</b>  Essence/Petrol</p>
+															<p><b>Engine Cap:</b>  n/a</p>
+															<p><b>Registration Date:</b>  Unknown</p>
+															<p><b>Year of Manufacture:</b>  2000</p>
+															<p><b>Mileage:</b>  270,000</p>
+															<p><b>Features:</b>  Unknown</p>
+															<p><b>Accessoriese:</b>  Radio</p>
+															<p><a href="tel:+250782782300" class="btn btn-primary">Call Seller</a>
+																<a href="mailto:lucasericofficial1@gmail.com" class="btn btn-primary">Email Seller</a>
+															<a href="https://api.whatsapp.com/send?phone=+250782782300&text=hello!%21%20i need%20to buy%20your car%20and need%20more information." class="btn btn-primary" target="_blank">
+															<i class="fa fa-whatsapp my-float">Whatsapp Seller</i>
+															</a></p>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+
+										
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+<?php
+include('footer.html');
+?>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
