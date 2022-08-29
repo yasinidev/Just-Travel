f953a2f78ed214bf9da5aaca9aec264c2f11a65b
yasiniweb <yasini4real@gmail.com>
2022-08-29 15:54:15 +0200

    Create tours.php

---
+++ tours.php
@@ -0,0 +1,215 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Just Travel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+	<?php
+	include('navbarservicespage.html');
+	?>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/Visit-Rwanda-Helicopter.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>Find Tours</h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+<div class="colorlib-tour colorlib-light-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+						<h2>Our Tour Destinations</h2>
+						<p>Experience the rwandan beauty with our tours.</p>
+					</div>
+				</div>
+			</div>
+			<div class="tour-wrap">
+				<a href="tour4.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour4.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 25 Reviews</p>
+						<h2>Akagera National Park.</h2>
+						<span class="city">Abundance of birds and antelope.</span>
+					</span>
+				</a>
+				<a href="tour1.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour1.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 45 Reviews</p>
+						<h2>Volcanoes National Park.</h2>
+						<span class="city">gorilla trekking at Volcanoes National Park.</span>
+					</span>
+				</a>
+				<a href="tour2.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour2.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 55 Reviews</p>
+						<h2>Nyungwe Forest National Park.</h2>
+						<span class="city">the canopy tour andthe park's 15 hiking trails.</span>
+					</span>
+				</a>
+				<a href="tour5.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour5.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 45 Reviews</p>
+						<h2>Lake Kivu.</h2>
+						<span class="city">there's no better place to relax than Lake Kivu.</span>
+					</span>
+				</a>
+				<a href="tour3.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour3.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 54 Reviews</p>
+						<h2>King's Palace Museum.</h2>
+						<span class="city">Showcases replica of palace from 15th century.</span>
+					</span>
+				</a>
+				<a href="tour6.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour9.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 25 Reviews</p>
+						<h2>Kigali Genocide Memorial.</h2>
+						<span class="city">Tourists can learn about this horrific event at the Kigali Genocide Memorial.</span>
+					</span>
+				</a>
+				
+				<a href="tour9.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour6.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 45 Reviews</p>
+						<h2>Explore Kigali.</h2>
+						<span class="city">Travelers spend a night here pre gorilla trekk.</span>
+					</span>
+				</a>
+				<a href="tour7.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour7.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 55 Reviews</p>
+						<h2>Gishwati National Park.</h2>
+						<span class="city">Guided chimp and monkey tracking tours.</span>
+					</span>
+				</a>
+				<a href="tour8.php" class="tour-entry animate-box">
+					<div class="tour-img" style="background-image: url(images/tour8.jpg);">
+					</div>
+					<span class="desc">
+						<p class="star"><span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span> 55 Reviews</p>
+						<h2>Gorilla Guardians Village.</h2>
+						<span class="city">A spot to immerse yourself in Rwandan culture.</span>
+					</span>
+				</a>
+			</div>
+		</div>
+
+<?php
+include('footer.html');
+?>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
